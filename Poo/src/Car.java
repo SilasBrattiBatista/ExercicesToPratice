b69e0156b5f568b3b797d3190a5a6f07afa4eb8a
Silas <silas.bratti.bratti@gmail.com>
2026-03-09 20:18:18 -0300

    Applying POO

---
+++ Poo/src/Car.java
@@ -0,0 +1,56 @@
+public class Car {
+    private String modelName;
+    private boolean year2022;
+    private boolean year2023;
+    private boolean year2024;
+    private double mediumPrice;
+
+    public String getModelName() {
+        return this.modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public boolean isYear2022() {
+        return year2022;
+    }
+
+    public void setYear2022(boolean year2022) {
+        this.year2022 = year2022;
+    }
+
+    public boolean isYear2023() {
+        return this.year2023;
+    }
+
+    public void setYear2023(boolean year2023) {
+        this.year2023 = year2023;
+    }
+
+    public boolean isYear2024() {
+        return this.year2024;
+    }
+
+    public void setYear2024(boolean year2024) {
+        this.year2024 = year2024;
+    }
+
+    public double getMediumPrice() {
+        return this.mediumPrice;
+    }
+
+    public void setMediumPrice(double mediumPrice) {
+
+        if (this.year2022) {
+            this.mediumPrice = 32.000;
+        } else if (this.year2023) {
+            this.mediumPrice = 65.000;
+        }  else if (this.year2024) {
+            this.mediumPrice = 125.000;
+        } else  {
+            System.out.println("It's a invalid year Model");
+        }
+    }
+}
