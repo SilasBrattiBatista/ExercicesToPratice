714aa5800ba107ee8d3a59324d5221b297d22119
Silas Bratti Batista <silas.bratti.bratti@gmail.com>
2026-03-16 13:25:09 -0300

    Practing POO

--- Poo/src/Main.java
+++ Poo/src/Main.java
@@ -1,3 +1,10 @@
+/*
+Create a Car class with methods to represent a specific model over three years. Implement methods to set the model name,
+average prices for each year, and calculate and display the lowest and highest prices. Add a subclass CarModel to
+create specific instances, using it in the main class to set prices and display information.*
+*/
+
+
 public class Main {
     public static void main(String[] args) {
         ModelCar unoMille = new ModelCar();
